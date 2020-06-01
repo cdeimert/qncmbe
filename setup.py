768d8245fc3dac1ca3e4c1eb814b0c1c644e4f7e
cdeimert <cdeimert@uwaterloo.ca>
2020-06-01 14:23:48 -0400

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qncmbe',
-    version='1.0',
+    version='1.1',
     description='Python tools for the QNC-MBE lab at '
                 'the University of Waterloo',
     url='https://github.com/cdeimert/qncmbe',
